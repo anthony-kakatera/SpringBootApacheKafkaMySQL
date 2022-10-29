fb0bc22821a2f90214f2067fa9e0e0d5e7cf03e8
Anthony Kakatera <anthonyckakatera@gmail.com>
2022-10-30 01:30:28 +0200

    Added sendMessage method in producer

--- SpringBootKafka/KafkaProducer/src/main/java/com/kafka/producer/KafkaProducer.java
+++ SpringBootKafka/KafkaProducer/src/main/java/com/kafka/producer/KafkaProducer.java
@@ -1,9 +1,14 @@
 package com.kafka.producer;
 
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class KafkaProducer {
@@ -16,9 +21,19 @@ public class KafkaProducer {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(){
+    public void sendMessage() throws InterruptedException {
         String topic = "wikimedia_recentchanges";
 
+        EventHandler eventHandler = new KafkaConsumer(topic, kafkaTemplate);
 
+        String source = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(source));
+
+        EventSource eventSource = builder.build();
+
+        eventSource.start();
+
+        TimeUnit.MINUTES.sleep(10);
     }
 }
