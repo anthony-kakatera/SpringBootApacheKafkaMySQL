7bf62a351b01d1ff1140c0bd391ee880b4079ba5
Anthony Kakatera <anthonyckakatera@gmail.com>
2022-10-30 17:56:12 +0200

    Added service to interact with consumer

--- SpringBootKafka/KafkaConsumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
+++ SpringBootKafka/KafkaConsumer/src/main/java/com/kafka/consumer/KafkaConsumer.java
@@ -1,5 +1,8 @@
 package com.kafka.consumer;
 
+import com.kafka.consumer.entity.WikimediaChanges;
+import com.kafka.consumer.repository.IWikimediaChangesRepository;
+import com.kafka.consumer.service.IWikimediaChangesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,6 +12,14 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
+    //private IWikimediaChangesRepository iWikimediaChangesRepository;
+
+    private IWikimediaChangesService iWikimediaChangesService;
+
+    public KafkaConsumer(IWikimediaChangesService iWikimediaChangesService) {
+        this.iWikimediaChangesService = iWikimediaChangesService;
+    }
+
     @KafkaListener(
             topics = "wikimedia_recentchanges",
             groupId = "localGroup"
@@ -16,6 +27,9 @@ public class KafkaConsumer {
     public void consumeMessage(String message){
         LOGGER.info(String.format("Event Received: %s", message));
 
+        WikimediaChanges wikimediaChanges = new WikimediaChanges();
+        wikimediaChanges.setData(message);
 
+        iWikimediaChangesService.save(wikimediaChanges);
     }
 }
